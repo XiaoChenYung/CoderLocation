4297bc45fe06196d14be6f99e52d754e8a304443
XiaoChenYung <18730222889@126.com>
2015-11-03 22:01:54 +0800

    实现解码

---
+++ CoderLocation/CoderLocation/ViewController.m
@@ -0,0 +1,56 @@
+//
+//  ViewController.m
+//  CoderLocation
+//
+//  Created by 杨晓晨 on 15/11/3.
+//  Copyright © 2015年 杨晓晨. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface ViewController ()
+- (IBAction)bianma:(UIButton *)sender;
+
+- (IBAction)jiema:(UIButton *)sender;
+
+@property (nonatomic ,strong) CLGeocoder *geoCoder;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -   lazy
+
+- (IBAction)bianma:(UIButton *)sender {
+    NSString *str = @"北京";
+    
+    [self.geoCoder geocodeAddressString:str completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        for (CLPlacemark *pl in placemarks) {
+            CLLocation *loc = pl.location;
+            
+            NSLog(@"%@---%f-----%f",pl.name,loc.coordinate.latitude,loc.coordinate.longitude);
+        }
+    }];
+}
+
+- (IBAction)jiema:(UIButton *)sender {
+}
+
+- (CLGeocoder *)geoCoder {
+    if (_geoCoder == nil) {
+        _geoCoder = [[CLGeocoder alloc] init];
+    }
+    return _geoCoder;
+}
+
+@end
